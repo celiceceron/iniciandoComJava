f2bae969cfe49edce55a0cd404be5640934f8b14
celiceceron <celice.cer@gmail.com>
2022-10-09 00:40:19 +0200

    alterando  o java

--- iniciandocomjava/javaaula1.js
+++ iniciandocomjava/javaaula1.js
@@ -1,9 +1,21 @@
 //  remetente
+//  valor do produto
 // destinatario
 // assunto
 // corpo
 
 let remetente 
+let ValorDoProduto
+let destinatario
+let corpo
+
+// coloca o  valor  "celice.cer@gmail.com" dentro do remetente
 remetente  = "celice.cer@gmail.com"
+destinatario = "celice.ceron@hotmail.com"
+assunto = "Estou online no bootcamp"
+corpo = "Ei, tudo bem? Estou online, nao posso ajudar"
 
 console.log(remetente)
+console.log(destinatario)
+console.log(assunto)
+console.log(corpo)
